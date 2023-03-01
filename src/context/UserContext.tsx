0638694b0256a42b2b4f7f3a5577a55b9970c5a0
prasenjitpawar22 <prasenjitpawar22@gmail.com>
2023-03-01 20:06:13 +0530

    adding useEffect to userContext

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,29 +1,37 @@
-import React, { ReactNode, createContext, useState, Dispatch, SetStateAction } from "react";
-
+import React, { ReactNode, createContext, useState, Dispatch, SetStateAction, useEffect } from "react";
+import {LoginRequest} from '../api/UserApi'
 interface UserContextInterface {
-    user: User
-    setUser: Dispatch<SetStateAction<User>>
+  user: User
+  setUser: Dispatch<SetStateAction<User>>
 }
 
 export const UserContext = createContext<Partial<UserContextInterface>>({})
 
 type UserProviderProps = {
-    children: ReactNode
+  children: ReactNode
 }
 
 type User = {
-    username: string|undefined
-    _id: string|undefined
-    email: string|undefined
+  username: string | undefined
+  _id: string | undefined
+  email: string | undefined
 }
 
 
 export const UserProvider = ({ children }: UserProviderProps) => {
-    const [user, setUser] = useState<User>({ _id: '', email: '', username: '' })
+  const [user, setUser] = useState<User>({ _id: '', email: '', username: '' })
 
-    return (
-        <UserContext.Provider value={{ user, setUser }}>
-            {children}
-        </UserContext.Provider>
-    )
+  useEffect(() => {
+    const requestUser = async() => {
+      // const res = await LoginRequest()
+    }
+
+  }, [])
+
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  )
 }
