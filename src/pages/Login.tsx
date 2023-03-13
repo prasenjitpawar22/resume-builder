2871d6ad4b6dd18d4de3ef38876983c42feda7b1
Prasenjit Pawar <prasenjit.pawar@capgemini.com>
2023-03-13 15:03:19 +0530

    fixing deploy

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -20,8 +20,8 @@ const Login = () => {
 
 
   useEffect(() => {
-    // console.log(process.env.REACT_APP_API_URL);
-    // console.log(process.env.REACT_APP_BACKEND);
+    console.log(process.env.REACT_APP_API_URL);
+    console.log(process.env.REACT_APP_BACKEND);
 
     if (user?.logedIn) {
       navigate('/')
