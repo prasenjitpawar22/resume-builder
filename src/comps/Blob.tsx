566a50eb42cfe12ce1f629887ee8e568f02e11d0
Prasenjit Pawar <prasenjit.pawar@capgemini.com>
2023-03-16 19:00:43 +0530

    working on landing page

---
+++ src/comps/Blob.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type TProps = {
+}
+
+const Blob: React.FC<TProps> = () => {
+  return (
+    <img src={'./svgs/blob1.svg'} alt="Your SVG" width={500} />
+  )
+}
+
+export default Blob
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { redirect, useNavigate } from 'react-router-dom'
+import { UserContext } from '../context/UserContext'
+import Blob from '../comps/Blob'
 
 type TProps = {}
 const Home: React.FC<TProps> = () => {
     const navigate = useNavigate()
+    const { user } = useContext(UserContext)
 
     return (
-        <div>
-            <button onClick={() => {
-                navigate('/build')
-            }}
-            >build</button>
+        <div className='min-h-screen font-Lato flex items-center justify-center'>
+            <div className='z-10 flex flex-col gap-4 justify-center items-center'>
+                <h2 className="font-Lato phone:text-3xl tablet:text-7xl text-primary text-center
+               tablet:font-extrabold" style={{ textShadow: 'rgb(0 0 0 / 25%) -2px 4px 5px' }}>
+                    Resume Builder
+                </h2>
+                <button onClick={() => { user?.logedIn ? navigate('/build') : navigate('/login') }}
+                    className='bg-component-secondary p-3 rounded text-slate-100
+                font-bold hover:bg-component-primary transition-all duration-300'>
+                    Build Resume
+                </button>
+            </div>
+            <div className='absolute w-full h-full overflow-hidden'>
+                <Blob />
+            </div>
         </div>
     )
 }
